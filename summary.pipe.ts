1e3539634bcf064065416b872cfcbbcf0f1f1475
chrisjune <chrisjune2701@gmail.com>
2017-10-25 00:24:23 +0900

    first commit

---
+++ summary.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+  transform(value: string, limit?: number) {
+      if (!value) {
+        return null;
+      }
+      let actualLimit = (limit) ? limit : 10;
+      return value.substring(0, actualLimit) + '....';
+  }
+}
